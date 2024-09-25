1f069778fdbca8fdb9f04e9a41e364fa73a7822c
jmgdls <jazzymae.delossantos@gmail.com>
2024-09-25 11:48:27 -0700

    Try to fix 502 error, removed .venv

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -22,6 +22,18 @@ def get_catalog():
     Each unique item combination must have only a single price.
     """
 
+    """
+    inside return[]:
+    {
+        "sku": "string", /* Matching regex ^[a-zA-Z0-9_]{1,20}$ */
+        "name": "string",
+        "quantity": "integer", /* Between 1 and 10000 */
+        "price": "integer", /* Between 1 and 500 */
+        "potion_type": [r, g, b, d] /* r, g, b, d are integers that add up to exactly 100 */
+    }
+    seperate multiple entries with comma (,)
+    """
+
     return [
             {
                 "sku": "RED_POTION_0",
@@ -29,5 +41,15 @@ def get_catalog():
                 "quantity": 1,
                 "price": 50,
                 "potion_type": [100, 0, 0, 0],
+            },
+
+            {
+                "sku": "GREEN_POTION_0",
+                "name": "green potion",
+                "quantity": 1,
+                "price": 50,
+                "potion_type": [0, 100, 0, 0],
             }
         ]
+
+    
